18f4a4087f99339d18d4ef7cd521d76597f2682f
Ryan Rempel <rgrempel@gmail.com>
2018-04-06 22:28:04 -0500

    Add map6, map7 and map8.

--- src/Elm/Apply.purs
+++ src/Elm/Apply.purs
@@ -6,44 +6,64 @@
 -- | `lift5`. We also re-export these in the individual Elm modules that use
 -- | them, so that that the API matches with the Elm API.
 -- |
+-- | A few Elm modules use up to `map8`, so we implement those here as well.
+-- |
 -- | We also make `andMap` a synonym for Purescript's `apply`.
 
 module Elm.Apply
     ( andMap
-    , map2, map3, map4, map5
+    , map2, map3, map4, map5, map6, map7, map8
     ) where
 
 
 import Control.Apply (lift2, lift3, lift4, lift5)
-import Prelude (class Apply, apply)
+import Prelude (class Apply, apply, (<$>), (<*>))
 
 
 -- | Map a function of two arguments over some container type.
 -- |
 -- | The equivalent of Purescript's `lift2`.
-map2 :: ∀ a b c f. (Apply f) => (a -> b -> c) -> f a -> f b -> f c
+map2 :: ∀ w a b c. (Apply w) => (a -> b -> c) -> w a -> w b -> w c
 map2 = lift2
 
 
 -- | Map a function of three arguments over some container type.
 -- |
 -- | The equivalent of Purescript's `lift3`.
-map3 :: ∀ a b c d f. (Apply f) => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
+map3 :: ∀ w a b c d. (Apply w) => (a -> b -> c -> d) -> w a -> w b -> w c -> w d
 map3 = lift3
 
 
 -- | Map a function of four arguments over some container type.
 -- |
 -- | The equivalent of Purescript's `lift4`.
-map4 :: ∀ a b c d e f. (Apply f) => (a -> b -> c -> d -> e) -> f a -> f b -> f c -> f d -> f e
+map4 :: ∀ w a b c d e. (Apply w) => (a -> b -> c -> d -> e) -> w a -> w b -> w c -> w d -> w e
 map4 = lift4
 
 
 -- | Map a function of five arguments over some container type.
 -- |
 -- | The equivalent of Purescript's `lift5`.
-map5 :: ∀ a b c d e f g. (Apply f) => (a -> b -> c -> d -> e -> g) -> f a -> f b -> f c -> f d -> f e -> f g
+map5 :: ∀ w a b c d e f. (Apply w) => (a -> b -> c -> d -> e -> f) -> w a -> w b -> w c -> w d -> w e -> w f
 map5 = lift5
+
+
+-- | Map a function of six arguments over some container type.
+map6 :: ∀ w a b c d e f g. (Apply w) => (a -> b -> c -> d -> e -> f -> g) -> w a -> w b -> w c -> w d -> w e -> w f -> w g
+map6 func a b c d e f =
+    func <$> a <*> b <*> c <*> d <*> e <*> f
+
+
+-- | Map a function of seven arguments over some container type.
+map7 :: ∀ w a b c d e f g h. (Apply w) => (a -> b -> c -> d -> e -> f -> g -> h) -> w a -> w b -> w c -> w d -> w e -> w f -> w g -> w h
+map7 func a b c d e f g =
+    func <$> a <*> b <*> c <*> d <*> e <*> f <*> g
+
+
+-- | Map a function of eight arguments over some container type.
+map8 :: ∀ w a b c d e f g h i. (Apply w) => (a -> b -> c -> d -> e -> f -> g -> h -> i) -> w a -> w b -> w c -> w d -> w e -> w f -> w g -> w h -> w i
+map8 func a b c d e f g h =
+    func <$> a <*> b <*> c <*> d <*> e <*> f <*> g <*> h
 
 
 -- | Map a function in a container to a value in a container.
