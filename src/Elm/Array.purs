69e29c0c3d2b4dad320948c9104b19eb1702c239
Ryan Rempel <rgrempel@gmail.com>
2018-05-05 13:12:08 -0500

    Quote Elm docs, and use `Array` in signatures instead of `Seq`

--- src/Elm/Array.purs
+++ src/Elm/Array.purs
@@ -1,14 +1,18 @@
 
--- | A library for fast immutable arrays. The elements in an array must have the
--- | same type.
+-- | > A library for fast immutable arrays. The elements in an array must have the
+-- | > same type.
 -- |
--- | * This is based on the Purescript `Data.Sequence` package -- an `Array` is a
+-- | This is based on the Purescript `Data.Sequence` package -- an `Array` is a
 -- | `Data.Sequence.Seq`, so you can use additional functions from that package
--- | as well. *
+-- | as well.
 -- |
--- | * Note that the Purescript primitive `Array` type is something different --
--- | it is actually a Javascript array. In order to avoid conflicts with `Prim.Array`,
--- | many type signatures below actually use `Seq` insted of `Array`. *
+-- | Note that the Purescript primitive `Array` type is something different --
+-- | it is actually a Javascript array. So, if you're importing this unqualified,
+-- | you'll probably need to do something like:
+-- |
+-- |     import Prim hiding (Array)
+-- |
+-- | to avoid an ambiguity.
 
 module Elm.Array
     ( module Virtual
@@ -32,9 +36,7 @@ import Data.Sequence
     , snoc, index, null, take, drop, length
     )
 
--- This is supposed to prevent conflict with the locally-defined
--- array type, but it doesn't work in Purescript 0.9.1 ... you
--- get a conflict anyway.
+-- Prevents conflict with the locally-defined `Array` type.
 import Prim hiding (Array)
 
 import Elm.Basics (Bool)
@@ -53,19 +55,17 @@ import Prelude
     , ($), (-), const, flip, (>=), (<=), (+), (<<<), (<>)
     )
 
--- | The `Array` type is synonym for `Data.Sequence.Seq`. Note that many type
--- | signatures in this module use `Seq` instead of `Array` to avoid a conflict
--- | with `Prim.Array`.
+-- | The `Array` type is synonym for `Data.Sequence.Seq`.
 type Array = Seq
 
 
--- | Initialize an array. `initialize n f` creates an array of length `n` with
--- | the element at index `i` initialized to the result of `(f i)`.
--- |
--- |     initialize 4 identity    == fromList [0,1,2,3]
--- |     initialize 4 (\n -> n*n) == fromList [0,1,4,9]
--- |     initialize 4 (always 0)  == fromList [0,0,0,0]
-initialize :: ∀ a. Int -> (Int -> a) -> Seq a
+-- | > Initialize an array. `initialize n f` creates an array of length `n` with
+-- | > the element at index `i` initialized to the result of `(f i)`.
+-- | >
+-- | >     initialize 4 identity    == fromList [0,1,2,3]
+-- | >     initialize 4 (\n -> n*n) == fromList [0,1,4,9]
+-- | >     initialize 4 (always 0)  == fromList [0,0,0,0]
+initialize :: ∀ a. Int -> (Int -> a) -> Array a
 initialize len func =
     if len <= 0
        then empty
@@ -74,42 +74,42 @@ initialize len func =
                fromFoldable (DA.range 0 (len - 1))
 
 
--- | Creates an array with a given length, filled with a default element.
--- |
--- |     repeat 5 0     == fromList [0,0,0,0,0]
--- |     repeat 3 "cat" == fromList ["cat","cat","cat"]
--- |
--- | Notice that `repeat 3 x` is the same as `initialize 3 (always x)`.
-repeat :: ∀ a. Int -> a -> Seq a
+-- | > Creates an array with a given length, filled with a default element.
+-- | >
+-- | >     repeat 5 0     == fromList [0,0,0,0,0]
+-- | >     repeat 3 "cat" == fromList ["cat","cat","cat"]
+-- | >
+-- | > Notice that `repeat 3 x` is the same as `initialize 3 (always x)`.
+repeat :: ∀ a. Int -> a -> Array a
 repeat n e =
     initialize n (const e)
 
 
--- | Create an array from a list.
+-- | > Create an array from a list.
 -- |
 -- | Note that this actually works with any `Foldable`.
-fromList :: ∀ f a. (Foldable f) => f a -> Seq a
+fromList :: ∀ f a. (Foldable f) => f a -> Array a
 fromList = fromFoldable
 
 
--- | Create a list of elements from an array.
--- |
--- |     toList (fromList [3,5,8]) == [3,5,8]
+-- | > Create a list of elements from an array.
+-- | >
+-- | >     toList (fromList [3,5,8]) == [3,5,8]
 -- |
 -- | Note that this actually works with any type that is both a
 -- | `Functor` and an `Unfoldable`.
-toList :: ∀ f a. Functor f => Unfoldable f => Seq a -> f a
+toList :: ∀ f a. Functor f => Unfoldable f => Array a -> f a
 toList = toUnfoldable
 
 
--- | Create an indexed list from an array. Each element of the array will be
--- | paired with its index.
--- |
--- |     toIndexedList (fromList ["cat","dog"]) == [Tuple 0 "cat", Tuple 1 "dog"]
+-- | > Create an indexed list from an array. Each element of the array will be
+-- | > paired with its index.
+-- | >
+-- | >     toIndexedList (fromList ["cat","dog"]) == [Tuple 0 "cat", Tuple 1 "dog"]
 -- |
 -- | The container in the return type is defined polymorphically to accommodate
 -- | `List` and Purescript's `Array`, among others.
-toIndexedList :: ∀ f a. Applicative f => Monoid (f (Tuple Int a)) => Seq a -> f (Tuple Int a)
+toIndexedList :: ∀ f a. Applicative f => Monoid (f (Tuple Int a)) => Array a -> f (Tuple Int a)
 toIndexedList arr =
     snd $ Foldable.foldr step (Tuple ((length arr) - 1) mempty) arr
         where
@@ -117,10 +117,10 @@ toIndexedList arr =
                 Tuple (index - 1) ((pure $ Tuple index item) <> list)
 
 
--- | Apply a function on every element with its index as first argument.
--- |
--- |     indexedMap (*) (fromList [5,5,5]) == fromList [0,5,10]
-indexedMap :: ∀ a b. (Int -> a -> b) -> Seq a -> Seq b
+-- | > Apply a function on every element with its index as first argument.
+-- | >
+-- | >     indexedMap (*) (fromList [5,5,5]) == fromList [0,5,10]
+indexedMap :: ∀ a b. (Int -> a -> b) -> Array a -> Array b
 indexedMap func =
     snd <<< Foldable.foldl step (Tuple 0 empty)
         where
@@ -128,53 +128,53 @@ indexedMap func =
                 Tuple (index + 1) (snoc seq (func index item))
 
 
--- | Push an element to the end of an array.
--- |
--- |     push 3 (fromList [1,2]) == fromList [1,2,3]
+-- | > Push an element to the end of an array.
+-- | >
+-- | >     push 3 (fromList [1,2]) == fromList [1,2,3]
 -- |
 -- | Equivalent to Purescript's `snoc`, but with the arguments flipped.
-push :: ∀ a. a -> Seq a -> Seq a
+push :: ∀ a. a -> Array a -> Array a
 push = flip snoc
 
 
--- | Return Just the element at the index or Nothing if the index is out of range.
--- |
--- |     get  0 (fromList [0,5,3]) == Just 0
--- |     get  2 (fromList [0,5,3]) == Just 3
--- |     get  5 (fromList [0,5,3]) == Nothing
--- |     get -1 (fromList [0,5,3]) == Nothing
+-- | > Return Just the element at the index or Nothing if the index is out of range.
+-- | >
+-- | >     get  0 (fromList [0,5,3]) == Just 0
+-- | >     get  2 (fromList [0,5,3]) == Just 3
+-- | >     get  5 (fromList [0,5,3]) == Nothing
+-- | >     get -1 (fromList [0,5,3]) == Nothing
 -- |
 -- | Equivalent to Purescript's `index`.
-get :: ∀ a. Int -> Seq a -> Maybe a
+get :: ∀ a. Int -> Array a -> Maybe a
 get = index
 
 
--- | Set the element at a particular index. Returns an updated array.
--- | If the index is out of range, the array is unaltered.
--- |
--- |     set 1 7 (fromList [1,2,3]) == fromList [1,7,3]
+-- | > Set the element at a particular index. Returns an updated array.
+-- | > If the index is out of range, the array is unaltered.
+-- | >
+-- | >     set 1 7 (fromList [1,2,3]) == fromList [1,7,3]
 -- |
 -- | Equivalent to Purescript's `replace`, but with the arguments flipped.
-set :: ∀ a. Int -> a -> Seq a -> Seq a
+set :: ∀ a. Int -> a -> Array a -> Array a
 set = flip replace
 
 
--- | Get a sub-section of an array: `(slice start end array)`. The `start` is a
--- | zero-based index where we will start our slice. The `end` is a zero-based index
--- | that indicates the end of the slice. The slice extracts up to but not including
--- | `end`.
--- |
--- |     slice  0  3 (fromList [0,1,2,3,4]) == fromList [0,1,2]
--- |     slice  1  4 (fromList [0,1,2,3,4]) == fromList [1,2,3]
--- |
--- | Both the `start` and `end` indexes can be negative, indicating an offset from
--- | the end of the array.
--- |
--- |     slice  1 -1 (fromList [0,1,2,3,4]) == fromList [1,2,3]
--- |     slice -2  5 (fromList [0,1,2,3,4]) == fromList [3,4]
--- |
--- | This makes it pretty easy to `pop` the last element off of an array: `slice 0 -1 array`
-slice :: ∀ a. Int -> Int -> Seq a -> Seq a
+-- | > Get a sub-section of an array: `(slice start end array)`. The `start` is a
+-- | > zero-based index where we will start our slice. The `end` is a zero-based index
+-- | > that indicates the end of the slice. The slice extracts up to but not including
+-- | > `end`.
+-- | >
+-- | >     slice  0  3 (fromList [0,1,2,3,4]) == fromList [0,1,2]
+-- | >     slice  1  4 (fromList [0,1,2,3,4]) == fromList [1,2,3]
+-- | >
+-- | > Both the `start` and `end` indexes can be negative, indicating an offset from
+-- | > the end of the array.
+-- | >
+-- | >     slice  1 -1 (fromList [0,1,2,3,4]) == fromList [1,2,3]
+-- | >     slice -2  5 (fromList [0,1,2,3,4]) == fromList [3,4]
+-- | >
+-- | > This makes it pretty easy to `pop` the last element off of an array: `slice 0 -1 array`
+slice :: ∀ a. Int -> Int -> Array a -> Array a
 slice start end array =
     let
         len =
@@ -199,10 +199,10 @@ slice start end array =
         take toTake $ drop toDrop array
 
 
--- | Determine if an array is empty.
--- |
--- |     isEmpty empty == true
+-- | > Determine if an array is empty.
+-- | >
+-- | >     isEmpty empty == true
 -- |
 -- | Equivalent to Purescript's `null`.
-isEmpty :: ∀ a. Seq a -> Bool
+isEmpty :: ∀ a. Array a -> Bool
 isEmpty = null
